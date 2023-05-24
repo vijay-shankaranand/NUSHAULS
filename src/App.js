98f5059dfb652cfaaadfe52524405a53ec2464c9
Shankar Anand <shankaranand48@gmail.com>
2023-05-24 11:28:24 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div>
+      <h1>NUSHAULS</h1>
+      <Button type= "primary">Log in</Button>
+    </div>
+  );
+}
+
+export default App;

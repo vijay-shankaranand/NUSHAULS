1bbc602b8e353983e8c4d8a4d2c7672863a2b365
Shankar Anand <shankaranand48@gmail.com>
2023-06-19 22:59:49 +0800

    emailverify pg

--- frontend/src/page/EmailVerify.js
+++ frontend/src/page/EmailVerify.js
@@ -1,6 +1,7 @@
 import React, { useState , useEffect, Fragment} from "react";
 import { useParams, Link } from "react-router-dom";
 import axios from "axios"
+import success from '../asset/success.png'
 
 
 const EmailVerify = () => {
@@ -21,21 +22,26 @@ const EmailVerify = () => {
 		};
 		verifyEmailUrl();
 	}, [param]);
+  
   return (
     <Fragment>
 			{validUrl ? (
-				<div className="p-80 wd-40 flex items-center h-full justify-center items-center">
-					<p>Email verified successfully!</p>
+		  <div className="flex flex-col min-h-screen justify-center items-center">
+			<div>
+			  <img src={success} alt="Success" class="object-scale-down "/>
+			</div>
+			<div className="text-center text-bold text-2xl">
+			  <p className="pb-5">Email verified successfully!</p>
 					<Link to="/login">
-							<button className="rounded bg-amber-500 hover:bg-blue-800">Login</button>
+				<button className="rounded bg-amber-500 hover:bg-blue-800 w-20">Login</button>
 					</Link>
-
+			</div>
 				</div>
 			) : (
-				<h1>404 Not Found</h1>
+		  <h1 className="text-center">404 NOT FOUND. You have already verified your email or incorrect URL</h1>
 			)}
 		</Fragment>
-  )
-}
+	);
+  };
 
-export default EmailVerify
+  export default EmailVerify;
